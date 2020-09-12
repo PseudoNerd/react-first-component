fe8cbd909ac0c2ec9bf61628effb9dace5395389
pseudoNerd <tkb9721@gmail.com>
2020-09-13 03:42:16 +0530

    import and exports in create-react-app

---
+++ cra/sec-app/src/Dog.js
@@ -0,0 +1,15 @@
+import { render } from '@testing-library/react';
+import React, {Component} from 'react';
+import './Dog.css';
+
+class Dog extends Component {
+    render(){
+        return(
+            <div className="Dog">
+                <h1>Dog</h1>
+            </div>
+        );        
+    }
+}
+
+export default Dog;

a7f033f0bcc13f7165f9665403fcd432b291183f
pseudoNerd <tkb9721@gmail.com>
2020-09-12 22:55:00 +0530

    Styling react component

--- props/slots/slots.js
+++ props/slots/slots.js
@@ -3,8 +3,16 @@ class Slots extends React.Component {
         console.log(this.props);
         const {s1,s2,s3} = this.props;
         const win = (s1===s2) && (s2===s3);
+        const colors =  { 
+                            fontSize:'40px',
+                            backgroundColor:'white',
+                            border:'2px solid black'
+                        }
         return (
-            <div>  
+            <div className="Slots">
+                <p style={colors}>
+                    {s1} {s2} {s3}
+                </p>  
                 <h1>{win ? 'w' : 'l'}</h1>
             </div>
         )
